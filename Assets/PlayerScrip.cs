ffa8dcde4920526e1ae4aca945598f1c3040e075
parth mirjulkar <3dparth@gmail.com>
2020-10-29 23:28:00 +0530

    first game commit

---
+++ Assets/PlayerScrip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScrip : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 ve3force;
+
+    [SerializeField]
+    KeyCode keyright;
+
+    [SerializeField]
+    KeyCode keyleft;
+
+    void Update()
+    {
+        if (Input.GetKey(keyright))
+        GetComponent<Rigidbody>().velocity += ve3force;
+
+        if(Input.GetKey(keyleft))
+        GetComponent<Rigidbody>().velocity -= ve3force;
+    }
+}
---
+++ Assets/addPlayerControlerVelocity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayercontrolVelocity: MonoBehaviour
+{
+    [SerializeField]
+    Vector3 ve3force;
+
+    [SerializeField]
+    KeyCode right;
+
+    [SerializeField]
+    KeyCode keyleft; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity += ve3force;
+    }
+}
